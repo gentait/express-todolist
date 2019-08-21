56a8229f9124d717582d29d57578454f71e9eb46
Genta Ito <itogenta.toge@gmail.com>
2019-08-21 22:56:01 +0900

    JSONを返す

--- index.js
+++ index.js
@@ -4,9 +4,17 @@ const express = require("express");
 //expressアプリを生成する
 const app = express();
 
-//ルート(http://localhost/)にアクセスしてきたときに「Hello」を返す
-app.get("/", (req, res) => {
-  res.send("Hello");
+// http://localhost:3000/api/v1/list にアクセスしてきたときにTODOリストを返す
+app.get("/api/v1/list", (req, res) => {
+  // クライアントに送るJSONデータ
+  const todoList = [
+    { title: "JavaScriptを勉強する", done: true },
+    { title: "Node.jsを勉強する", done: false },
+    { title: "Web APIを作る", done: false }
+  ];
+
+  // JSONを送信する
+  res.json(todoList);
 });
 
 //ポート3000でサーバを立てる
