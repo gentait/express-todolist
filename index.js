d9770d6533c7736bfa25e8b0798b95fddd1de636
Genta Ito <itogenta.toge@gmail.com>
2019-08-21 21:42:38 +0900

    hello world

---
+++ index.js
@@ -0,0 +1,15 @@
+//expressモジュールを読み込む
+const express = require("express");
+
+//expressアプリを生成する
+const app = express();
+
+//ルート(http://localhost/)にアクセスしてきたときに「Hello」を返す
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+//ポート3000でサーバを立てる
+app.listen(3000, () => {
+  console.log("server is running on localhost:3000");
+});
