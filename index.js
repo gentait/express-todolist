650e0647324b3a34111121a137da088a09967777
Genta Ito <itogenta.toge@gmail.com>
2019-08-24 10:04:43 +0900

    deleteボタンの実装

--- index.js
+++ index.js
@@ -1,22 +1,55 @@
-//expressモジュールを読み込む
 const express = require("express");
+const multer = require("multer");
+const uuidv4 = require("uuid/v4");
 
 //expressアプリを生成する
 const app = express();
-
+app.use(multer().none());
 app.use(express.static("web"));
+
+const todoList = [];
 
 // http://localhost:3000/api/v1/list にアクセスしてきたときにTODOリストを返す
 app.get("/api/v1/list", (req, res) => {
-  // クライアントに送るJSONデータ
-  const todoList = [
-    { title: "JavaScriptを勉強する", done: true },
-    { title: "Node.jsを勉強する", done: false },
-    { title: "Web APIを作る", done: false }
-  ];
-
   // JSONを送信する
   res.json(todoList);
+});
+
+//http://localhost:3000/api/v1/addにデータを送信してきたときにTODOリストに項目を追加する
+app.post("/api/v1/add", (req, res) => {
+  //クライアントからの送信データを追加する
+  const todoData = req.body;
+  const todoTitle = todoData.title;
+
+  //ユニークIDを生成する
+  const id = uuidv4();
+
+  //TODO項目を作る
+  const todoItem = {
+    id,
+    title: todoTitle,
+    done: false
+  };
+  //TODO項目を追加する
+  todoList.push(todoItem);
+
+  console.log("Add:" + JSON.stringify(todoItem));
+
+  //追加した項目をクライアントに返す
+  res.json(todoItem);
+});
+
+//http://localhost:3000/api/v1/item/:idにDELETEで送信してきた時に項目を削除する
+app.delete("/api/v1/item/:id", (req, res) => {
+  console.log(req.params.id);
+  const index = todoList.findIndex(item => {
+    return item.id === req.params.id;
+  });
+  if (index >= 0) {
+    const deleted = todoList.splice(index, 1);
+    console.log(`Deleted: ${JSON.stringify(deleted[0])}`);
+  }
+  res.sendStatus(200);
 });
 
 //ポート3000でサーバを立てる
